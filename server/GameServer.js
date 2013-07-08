f141a646f22450a07c45c1cf7a3a5db20e93407d
Axel Hernández Ferrera <axelhzf@gmail.com>
2013-07-08 18:32:45 +0100

    fixed random

--- server/GameServer.js
+++ server/GameServer.js
@@ -142,8 +142,8 @@ GameServer.prototype = {
 
     addFood : function () {
         if (this.foodCollection.length < 10) {
-            var x = _.random(0, this.board.get('x'));
-            var y = _.random(0, this.board.get('y'));
+            var x = _.random(0, this.board.get('x') - 1);
+            var y = _.random(0, this.board.get('y') - 1);
             this.foodCollection.add({x : x, y : y});
         }
     },
