521f172705b5aa7bc6a4c0ee6384e363c865fe51
Axel Hernández Ferrera <axelhzf@gmail.com>
2013-07-16 21:11:59 +0100

    slides

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/server/views');
 
 app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
